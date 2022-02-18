f91a367c332e03dde1dab4d7660bcc5342533e7c
onowdev <onowdev@gmail.com>
2022-02-18 21:54:09 +0700

    pandas read head file only

---
+++ readfile/PdReadHead.py
@@ -0,0 +1,16 @@
+'''
+Membaca file dengan menggunakan head()
+Pada suatu kasus, data yang kita baca 
+cukup banyak atau loading yang lama. 
+Untuk memastikan data kita terbaca 
+dengan baik dan bisa menampilkan data 
+sebagian untuk ditampilkan secara benar, 
+kita bisa memakai fungsi head(). 
+Bisa dituliskan kode di bawah ini untuk prakteknya:
+'''
+
+import pandas as pd
+
+csv_data = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/shopping_data.csv")
+
+print(csv_data.head())
